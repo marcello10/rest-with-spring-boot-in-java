5b7d3d638fcd7a841b9ba2ddbde29720695e175d
Marcelo <marcello159357@gmail.com>
2024-02-15 08:41:33 -0300

    Inicio

---
+++ src/main/java/br/com/marcelo/restwithspringbootinjava/GreetingController.java
@@ -0,0 +1,2 @@
+package br.com.marcelo.restwithspringbootinjava;public class GreetingController {
+}
---
+++ src/main/java/br/com/marcelo/restwithspringbootinjava/Startup.java
@@ -0,0 +1,13 @@
+package br.com.marcelo.restwithspringbootinjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBootInJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringBootInJavaApplication.class, args);
+	}
+
+}
