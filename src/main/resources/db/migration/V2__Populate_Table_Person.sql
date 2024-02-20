28a36e875eb65a7d28e7f990dd32b28b1484ed46
Marcelo <marcello159357@gmail.com>
2024-02-20 09:12:49 -0300

    Utilizando Migrations e Flyway

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'São Paulo', 'Ayrton', 'Male', 'Senna'),
+	(3, 'Teresina', 'Maria', 'Female', 'Valentina'),
+	(4, 'Teresina', 'Maria', 'Female', 'das Graças'),
+	(7, 'Teresina', 'Marcelo', 'Male', 'Victor');
